8de8d47ffcc2bd589b32ae0a90362b8a2927f1c7
DB <DB@DB>
2020-11-14 14:41:10 +0700

    add frame size(450, 590)

--- SOF203/src/Lab3/StudentRegistration.java
+++ SOF203/src/Lab3/StudentRegistration.java
@@ -22,20 +22,20 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
-public class StudentRegistration extends JFrame implements ActionListener{
+public class StudentRegistration extends JFrame implements ActionListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8110432818367970093L;
-	
+
 	private JPanel pnlPersonal, pnlAcamedic, pnlExtracurricular, pnlButton;
-	private JTextField txtFirstName, txtLastName, txtContact, txtHigh, txtSpecification, txtEnroll, txtHobbies, txtSport;
+	private JTextField txtFirstName, txtLastName, txtContact, txtHigh, txtSpecification, txtEnroll, txtHobbies,
+			txtSport;
 	private JTextArea txtAddress;
 	private JButton btnSave, btnReset, btnExit;
 	private JScrollPane scrollPane;
 
 	public StudentRegistration() {
-		getContentPane().setBackground(SystemColor.controlHighlight);
 		initializer();
 	}
 
@@ -44,17 +44,20 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
-		
+		getContentPane().setBackground(SystemColor.controlHighlight);
+		setSize(450, 590);
+
 		JLabel lblTitle = new JLabel("STUDENT REGISTRATION");
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setBounds(86, 6, 258, 25);
 		lblTitle.setForeground(Color.MAGENTA);
 		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 20));
 		getContentPane().add(lblTitle);
-		
+
 		pnlPersonal = new JPanel();
 		pnlPersonal.setBackground(SystemColor.controlHighlight);
-		pnlPersonal.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Personal Detail", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlPersonal.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Personal Detail",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		pnlPersonal.setBounds(6, 32, 422, 223);
 		getContentPane().add(pnlPersonal);
 		pnlPersonal.setLayout(null);
@@ -85,10 +88,11 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		txtAddress = new JTextArea();
 		txtAddress.setTabSize(0);
 		scrollPane.setViewportView(txtAddress);
-		
+
 		pnlAcamedic = new JPanel();
 		pnlAcamedic.setBackground(SystemColor.controlHighlight);
-		pnlAcamedic.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Acamedic Details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlAcamedic.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Acamedic Details",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		pnlAcamedic.setBounds(6, 267, 422, 120);
 		JLabel lblHigh = new JLabel("Highest Qualification");
 		lblHigh.setBounds(48, 24, 114, 16);
@@ -110,10 +114,11 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		pnlAcamedic.add(lblEnroll);
 		pnlAcamedic.add(txtEnroll);
 		getContentPane().add(pnlAcamedic);
-		
+
 		pnlExtracurricular = new JPanel();
 		pnlExtracurricular.setBackground(SystemColor.controlHighlight);
-		pnlExtracurricular.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Extracurricular Details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pnlExtracurricular.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null),
+				"Extracurricular Details", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		pnlExtracurricular.setBounds(6, 399, 422, 95);
 		JLabel lblHobbies = new JLabel("Hobbies");
 		lblHobbies.setBounds(48, 29, 47, 16);
@@ -129,7 +134,7 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		pnlExtracurricular.add(lblSport);
 		pnlExtracurricular.add(txtSport);
 		getContentPane().add(pnlExtracurricular);
-		
+
 		pnlButton = new JPanel();
 		pnlButton.setBackground(SystemColor.controlHighlight);
 		pnlButton.setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
@@ -142,7 +147,7 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		pnlButton.add(btnSave);
 		pnlButton.add(btnReset);
 		pnlButton.add(btnExit);
-		
+
 		btnSave.setActionCommand("save");
 		btnSave.addActionListener(this);
 		btnReset.setActionCommand("reset");
@@ -150,7 +155,7 @@ public class StudentRegistration extends JFrame implements ActionListener{
 		btnExit.setActionCommand("exit");
 		btnExit.addActionListener(this);
 	}
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -166,13 +171,13 @@ public class StudentRegistration extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equals("save")) {
+		if (e.getActionCommand().equals("save")) {
 			save();
 		}
-		if(e.getActionCommand().equals("reset")) {
+		if (e.getActionCommand().equals("reset")) {
 			reset();
 		}
-		if(e.getActionCommand().equals("exit")) {
+		if (e.getActionCommand().equals("exit")) {
 			System.exit(0);
 		}
 	}
@@ -190,47 +195,47 @@ public class StudentRegistration extends JFrame implements ActionListener{
 	}
 
 	private void save() {
-		if(txtFirstName.getText().isEmpty()) {
+		if (txtFirstName.getText().isEmpty()) {
 			txtFirstName.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtFirstName.requestFocus();
-		}else if(txtLastName.getText().isEmpty()){
-			txtFirstName.setBackground(null);
+		} else if (txtLastName.getText().isEmpty()) {
+			txtFirstName.setBackground(Color.WHITE);
 			txtLastName.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtLastName.requestFocus();
-		}else if(txtContact.getText().isEmpty()){
-			txtLastName.setBackground(null);
+		} else if (txtContact.getText().isEmpty()) {
+			txtLastName.setBackground(Color.WHITE);
 			txtContact.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtContact.requestFocus();
-		}else if(txtAddress.getText().isEmpty()){
-			txtContact.setBackground(null);
+		} else if (txtAddress.getText().isEmpty()) {
+			txtContact.setBackground(Color.WHITE);
 			txtAddress.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtAddress.requestFocus();
-		}else if(txtHigh.getText().isEmpty()){
-			txtAddress.setBackground(null);
+		} else if (txtHigh.getText().isEmpty()) {
+			txtAddress.setBackground(Color.WHITE);
 			txtHigh.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtHigh.requestFocus();
-		}else if(txtSpecification.getText().isEmpty()){
-			txtHigh.setBackground(null);
+		} else if (txtSpecification.getText().isEmpty()) {
+			txtHigh.setBackground(Color.WHITE);
 			txtSpecification.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtSpecification.requestFocus();
-		}else if(txtEnroll.getText().isEmpty()){
-			txtSpecification.setBackground(null);
+		} else if (txtEnroll.getText().isEmpty()) {
+			txtSpecification.setBackground(Color.WHITE);
 			txtEnroll.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtEnroll.requestFocus();
-		}else if(txtHobbies.getText().isEmpty()){
-			txtEnroll.setBackground(null);
+		} else if (txtHobbies.getText().isEmpty()) {
+			txtEnroll.setBackground(Color.WHITE);
 			txtHobbies.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtHobbies.requestFocus();
-		}else if(txtSport.getText().isEmpty()){
-			txtHobbies.setBackground(null);
+		} else if (txtSport.getText().isEmpty()) {
+			txtHobbies.setBackground(Color.WHITE);
 			txtSport.setBackground(Color.YELLOW);
 			JOptionPane.showMessageDialog(this, "Please insert text");
 			txtSport.requestFocus();
