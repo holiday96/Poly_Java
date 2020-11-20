90a1ecae769ca06e3a0d6416c69213ba75c75dc1
DB <DB@DB>
2020-11-21 06:00:36 +0700

    completed

--- SOF203/src/Lab5/UserManagement.java
+++ SOF203/src/Lab5/UserManagement.java
@@ -20,8 +20,12 @@ import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
-public class UserManagement extends JFrame {
+public class UserManagement extends JFrame implements ActionListener {
 
 	/**
 	 * 
@@ -35,6 +39,10 @@ public class UserManagement extends JFrame {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JRadioButton rdbtnMale;
 	private JRadioButton rdbtnFemale;
+	private JButton btnBegin;
+	private JButton btnBack;
+	private JButton btnNext;
+	private JButton btnEnd;
 
 	private ArrayList<Student> list;
 	private int index;
@@ -60,14 +68,13 @@ public class UserManagement extends JFrame {
 	 */
 	public UserManagement() {
 		initialize();
-		list = new DAO().getListStudent();
 		index = 0;
 		try {
+			list = new DAO().getListStudent();
 			showInfo(index);
-		} catch (NullPointerException e) {
-			
+		} catch (IndexOutOfBoundsException e) {
+
 		}
-		showInfo(index);
 	}
 
 	/**
@@ -148,45 +155,176 @@ public class UserManagement extends JFrame {
 		scrollPane.setViewportView(txtAdress);
 
 		JButton btnAdd = new JButton("Add");
+		btnAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnAdd.setIcon(new ImageIcon(this.getClass().getResource("/images/add.png")));
+			}
+		});
+		btnAdd.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				btnAdd.setIcon(new ImageIcon(this.getClass().getResource("/images/add_hover.png")));
+			}
+		});
+		btnAdd.setHorizontalAlignment(SwingConstants.LEFT);
+		btnAdd.setIcon(new ImageIcon(this.getClass().getResource("/images/add.png")));
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				add();
 			}
 		});
-		btnAdd.setBounds(258, 12, 90, 28);
+		btnAdd.setBounds(258, 12, 109, 28);
 		panel.add(btnAdd);
 
 		JButton btnDelete = new JButton("Delete");
-		btnDelete.setBounds(258, 46, 90, 28);
+		btnDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnDelete.setIcon(new ImageIcon(this.getClass().getResource("/images/delete.png")));
+			}
+		});
+		btnDelete.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				btnDelete.setIcon(new ImageIcon(this.getClass().getResource("/images/delete_hover.png")));
+			}
+		});
+		btnDelete.setHorizontalAlignment(SwingConstants.LEFT);
+		btnDelete.setIcon(new ImageIcon(this.getClass().getResource("/images/delete.png")));
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				delete();
+			}
+		});
+		btnDelete.setBounds(258, 46, 109, 28);
 		panel.add(btnDelete);
 
 		JButton btnUpdate = new JButton("Update");
-		btnUpdate.setBounds(258, 80, 90, 28);
+		btnUpdate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnUpdate.setIcon(new ImageIcon(this.getClass().getResource("/images/write.png")));
+			}
+		});
+		btnUpdate.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				btnUpdate.setIcon(new ImageIcon(this.getClass().getResource("/images/write_hover.png")));
+			}
+		});
+		btnUpdate.setHorizontalAlignment(SwingConstants.LEFT);
+		btnUpdate.setIcon(new ImageIcon(this.getClass().getResource("/images/write.png")));
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				update();
+			}
+		});
+		btnUpdate.setBounds(258, 80, 109, 28);
 		panel.add(btnUpdate);
 
 		JButton btnSave = new JButton("Save");
-		btnSave.setBounds(258, 114, 90, 28);
+		btnSave.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		btnSave.setBounds(258, 114, 109, 28);
 		panel.add(btnSave);
 
-		JButton btnBegin = new JButton("Begin");
+		btnBegin = new JButton("Begin");
+		btnBegin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				begin();
+			}
+		});
 		btnBegin.setBounds(110, 269, 60, 28);
 		panel.add(btnBegin);
 
-		JButton btnBack = new JButton("Back");
+		btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				back();
+			}
+		});
 		btnBack.setBounds(177, 269, 55, 28);
 		panel.add(btnBack);
 
-		JButton btnNext = new JButton("Next");
-		btnNext.setBounds(238, 269, 52, 28);
+		btnNext = new JButton("Next");
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				next();
+			}
+		});
+		btnNext.setBounds(238, 269, 53, 28);
 		panel.add(btnNext);
 
-		JButton btnEnd = new JButton("End");
+		btnEnd = new JButton("End");
+		btnEnd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				end();
+			}
+		});
 		btnEnd.setBounds(298, 269, 50, 28);
 		panel.add(btnEnd);
 
 		JLabel lblAdress = new JLabel("Địa chỉ:");
 		lblAdress.setBounds(40, 193, 41, 16);
 		panel.add(lblAdress);
+	}
+
+	private void begin() {
+		index = 0;
+		showInfo(index);
+	}
+
+	private void end() {
+		index = list.size() - 1;
+		showInfo(index);
+	}
+
+	private void back() {
+		if (index > 0) {
+			index--;
+			showInfo(index);
+		}
+	}
+
+	private void next() {
+		if (index < list.size() - 1) {
+			index++;
+			showInfo(index);
+		}
+	}
+
+	protected void update() {
+		Student snew = new Student();
+		Student salt = list.get(index);
+		snew.setMaSV(txtID.getText());
+		snew.setHoTen(txtName.getText());
+		snew.setEmail(txtEmail.getText());
+		if (rdbtnMale.isSelected()) {
+			snew.setGioiTinh(true);
+		} else if (rdbtnFemale.isSelected()) {
+			snew.setGioiTinh(false);
+		}
+		snew.setSdt(txtPhone.getText());
+		snew.setDiaChi(txtAdress.getText());
+		if (new DAO().updateStudent(snew, salt)) {
+			JOptionPane.showMessageDialog(this, "Success");
+		} else {
+			JOptionPane.showMessageDialog(this, "Failed");
+		}
+	}
+
+	protected void delete() {
+		Student s = list.get(index);
+		if (new DAO().deleteStudent(s)) {
+			JOptionPane.showMessageDialog(this, "Success");
+		} else {
+			JOptionPane.showMessageDialog(this, "Failed");
+		}
 	}
 
 	private void add() {
@@ -220,5 +358,10 @@ public class UserManagement extends JFrame {
 		}
 		txtPhone.setText(list.get(index).getSdt());
 		txtAdress.setText(list.get(index).getDiaChi());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 }
