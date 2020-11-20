2cc1dcbdb41b13ba601b733656d33e4815bf159f
DB <DB@DB>
2020-11-21 06:01:23 +0700

    connect to database

--- SOF203/src/Lab5/DAO.java
+++ SOF203/src/Lab5/DAO.java
@@ -60,4 +60,49 @@ public class DAO {
 		}
 		return list;
 	}
+
+	public boolean deleteStudent(Student s) {
+		String sql = "DELETE FROM tblSTUDENT WHERE MASV = ? AND HOTEN = ? AND EMAIL = ? AND SDT = ? AND GIOITINH = ? AND DIACHI = ?";
+
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, s.getMaSV());
+			ps.setString(2, s.getHoTen());
+			ps.setString(3, s.getEmail());
+			ps.setString(4, s.getSdt());
+			ps.setBoolean(5, s.isGioiTinh());
+			ps.setString(6, s.getDiaChi());
+			return ps.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public boolean updateStudent(Student snew, Student salt) {
+		String sql = "UPDATE tblSTUDENT SET MASV = ?, HOTEN = ?, EMAIL = ?, SDT = ?, GIOITINH = ?, DIACHI = ? WHERE MASV = ? AND HOTEN = ? AND EMAIL = ? AND SDT = ? AND GIOITINH = ? AND DIACHI = ?";
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, snew.getMaSV());
+			ps.setString(2, snew.getHoTen());
+			ps.setString(3, snew.getEmail());
+			ps.setString(4, snew.getSdt());
+			ps.setBoolean(5, snew.isGioiTinh());
+			ps.setString(6, snew.getDiaChi());
+			
+			ps.setString(7, salt.getMaSV());
+			ps.setString(8, salt.getHoTen());
+			ps.setString(9, salt.getEmail());
+			ps.setString(10, salt.getSdt());
+			ps.setBoolean(11, salt.isGioiTinh());
+			ps.setString(12, salt.getDiaChi());
+			return ps.executeUpdate() > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
 }
