09fb35f07a419eb4d8c919e0527aaf56e3e3290c
Dute Bow <namlun96@gmail.com>
2021-05-02 15:36:27 +0700

    Delete test directory

--- test/src/main/java/test/dao/IUserDAO.java
+++
@@ -1,7 +0,0 @@
-package test.dao;
-
-import test.model.UserModel;
-
-public interface IUserDAO extends GenericDAO<UserModel> {
-	UserModel findByUserNameAndPasswordAndStatus(String userName, String password, Integer status);
-}
--- test/src/main/java/test/service/impl/NewService.java
+++
@@ -1,7 +0,0 @@
-package test.service.impl;
-
-import test.service.INewService;
-
-public class NewService extends INewService{
-
-}
