e9a658e01f4a7e8669e1b221dde58b10b298bbc9
XuShiTa <XuShiTa@192.168.1.17>
2021-08-29 23:30:57 +0700

    rename

--- SOF3021_Java5/src/main/java/com/java5/controller/lab/IndexLabController.java
+++ SOF3021_Java5/src/main/java/com/java5/controller/lab/IndexLabController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class LabController {
+public class IndexLabController {
 
 	@GetMapping("/lab")
 	public ModelAndView showLab() {
