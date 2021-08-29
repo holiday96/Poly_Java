8487ca35a3f4ab5527d3bca050c51bcfb38b439e
XuShiTa <XuShiTa@192.168.1.17>
2021-08-29 12:37:08 +0700

    rename class

--- SOF3021_Java5/src/main/java/com/java5/controller/lab/lab2/Lab2Bai2Controller.java
+++ SOF3021_Java5/src/main/java/com/java5/controller/lab/lab2/Lab2Bai2Controller.java
@@ -2,23 +2,26 @@ package com.java5.controller.lab.lab2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class Bai3Controller {
+public class Lab2Bai2Controller {
 
-	@GetMapping("/lab/lab2/bai3")
+	@GetMapping("/lab/lab2/bai2")
 	public ModelAndView showLab() {
-		ModelAndView mav = new ModelAndView("lab2bai3");
+		ModelAndView mav = new ModelAndView("lab2bai2");
 		return mav;
 	}
 	
-	@PostMapping("/lab/lab2/bai3/save")
-	public ModelAndView createProduct(@RequestParam String name, @RequestParam Long price) {
-		ModelAndView mav = new ModelAndView("lab2bai3");
-		mav.addObject("product", new Product(name, price));
+	@PostMapping("/lab/lab2/bai2/{y}")
+	public ModelAndView result(@RequestParam String x, @PathVariable String y) {
+		ModelAndView mav = new ModelAndView("lab2bai2");
+		mav.addObject("x", x);
+		mav.addObject("y", y);
 		return mav;
 	}
+	
 }
